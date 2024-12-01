ca087ba1c74269b147e53a40b9bc0cfcc9d76f38
Rafi Gemhar <gemhar@zoho.com>
2024-11-30 21:41:49 -0500

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
       "browser": true,
       "es2021": true,
-      "node": true
+      "node": true,
+      "jest": true  // This will fix the 'it' and 'expect' undefined errors
   },
   "extends": [
       "eslint:recommended",
