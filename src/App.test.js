fe2f83cc9a8d516ed881105bcd3b95273164d286
Rafi Gemhar <gemhar@zoho.com>
2024-12-09 20:37:33 -0500

    pass test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 /* eslint no-use-before-define: 0 */  // --> OFF
 
 // This test will fail
-it('should fail', () => {
+it('should pass', () => {
   expect(true).toBe(true);  // This will always pass
 });
 
 // This test will fail
-it('should fail', () => {
-  expect(true).toBe(false);  // This will always fail
-});
+// it('should fail', () => {
+//   expect(true).toBe(false);  // This will always fail
+// });
